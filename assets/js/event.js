1c800c8c9d306a02362a9d825aeb0d272d11715a
LunarTiger <lunartiger@live.com>
2020-02-07 13:03:20 -0500

    :tada:

--- assets/js/event.js
+++ assets/js/event.js
@@ -10,7 +10,7 @@ state.on('value', (function(snapshot) {
 		var id = database.ref('event-id');
 		id.on('value', (function(snapshot) {
 			var idVal = snapshot.val();
-			document.getElementById('nextEvent').innerHTML = "Next Event:&nbsp;&nbsp;<a href='https://sdq.st/e-"+idVal+"'>sdq.st/e-"+idval+"</a>"
+			document.getElementById('nextEvent').innerHTML = "Next Event:&nbsp;&nbsp;<a href='https://sdq.st/e-"+idVal+"'>sdq.st/e-"+idVal+"</a>"
 		}));
 	}
 }));
