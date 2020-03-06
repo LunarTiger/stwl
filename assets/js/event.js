faccd272fcbad3e0e82f465fd5be79dd8ec9c617
LunarTiger <lunartiger@live.com>
2020-03-06 06:11:14 -0500

    :tada:

--- assets/js/event.js
+++ assets/js/event.js
@@ -7,11 +7,7 @@ var event = database.ref('stwl/event');
 event.on('value', (function(snapshot) {
 	var eventVal = snapshot.val();
 	if(eventVal){
-		var id = database.ref('event-id');
-		id.on('value', (function(snapshot) {
-			var idVal = snapshot.val();
-			document.getElementById('nextEvent').innerHTML = "Next Event:&nbsp;&nbsp;<a href='https://sdq.st/e-"+idVal+"'>sdq.st/e-"+idVal+"</a>";
-		}));
+		document.getElementById('nextEvent').innerHTML = "Next Event:&nbsp;&nbsp;<a href='https://sdq.st/e-"+eventVal+"'>sdq.st/e-"+eventVal+"</a>";
 	}
 	if(!eventVal){
 		document.getElementById('nextEvent').innerHTML = "";
