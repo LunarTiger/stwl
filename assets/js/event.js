164f3fa5d2d5633058f64381540b2d4c67bad5b7
LunarTiger <lunartiger@live.com>
2020-03-06 05:51:33 -0500

    :tada:

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,19 +1,19 @@
 var config = {
-	databaseURL: "https://stwl-event.firebaseio.com/",
+	databaseURL: "https://lunar-home.firebaseio.com/",
 };
 firebase.initializeApp(config);
 var database = firebase.database();
-var state = database.ref('state');
-state.on('value', (function(snapshot) {
-	var stateVal = snapshot.val();
-	if(stateVal){
+var event = database.ref('stwl/event');
+event.on('value', (function(snapshot) {
+	var eventVal = snapshot.val();
+	if(eventVal){
 		var id = database.ref('event-id');
 		id.on('value', (function(snapshot) {
 			var idVal = snapshot.val();
 			document.getElementById('nextEvent').innerHTML = "Next Event:&nbsp;&nbsp;<a href='https://sdq.st/e-"+idVal+"'>sdq.st/e-"+idVal+"</a>";
 		}));
 	}
-	if(!stateVal){
+	if(!eventVal){
 		document.getElementById('nextEvent').innerHTML = "";
 	}
 }));
