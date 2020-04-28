01f728e9c88ad60d1f51504431e88ede241c4b56
LunarTiger <lunartiger@live.com>
2020-04-28 15:59:59 -0400

    :tada:

--- assets/js/event.js
+++ assets/js/event.js
@@ -7,9 +7,9 @@ var event = database.ref('stwl/next-event');
 event.on('value', (function(snapshot) {
 	var eventVal = snapshot.val();
 	if(eventVal){
-		document.getElementById('nextEvent').innerHTML = "Next Event:&nbsp;&nbsp;<a href='https://sdq.st/e-"+eventVal+"'>sdq.st/e-"+eventVal+"</a>";
+		document.getElementById('nextEvent').innerHTML = "Next Event:&nbsp; "+eventVal+"<br /><a href='https://discord.gg/DbQF7ze'>Join the discord.</a>";
 	}
 	if(!eventVal){
-		document.getElementById('nextEvent').innerHTML = "";
+		document.getElementById('nextEvent').innerHTML = "<a href='https://discord.gg/DbQF7ze'>Join the discord.</a>";
 	}
 }));
